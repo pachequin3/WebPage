3abda9e51dc0237f4a073dbceea5c4d1a6ff146d
tatianasuxo <tsuxos@est.emi.edu.bo>
2024-08-10 16:35:45 -0500

    ubic

--- src/componentsCont/ContactInfo.jsx
+++ src/componentsCont/ContactInfo.jsx
@@ -5,7 +5,7 @@ const ContactInfo = () => {
   return (
     <div className="contact-info">
       <h2>Dirección:</h2>
-      <p>Sopocachi Calle Pinilla entre 6 de Agosto y 20 de Octubre (plena esquina) TORRE VITRUVIO Piso 8 oficina 804</p>
+      <p>Sopocachi Calle Pinilla entre 6 de Agosto y 20 de Octubre (plena esquina) TORRE VITRUVIO Piso 8 oficina 804 La Paz, Bolivia</p>
     </div>
   );
 };
